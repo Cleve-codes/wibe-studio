fb18f98313d06b9974a3bdb057e0dabe3eb170b1
Cleve-codes <clevemomanyi@gmail.com>
2024-01-13 12:50:42 +0300

    nav done

--- src/components/CoverVideo.jsx
+++ src/components/CoverVideo.jsx
@@ -70,9 +70,7 @@ const container = {
   show:{
     opacity: .5,
 
-
-
-    transition:{
+    transition: {
      delayChildren: 2,
      staggerChildren: 0.3,
     }
@@ -95,35 +93,35 @@ const CoverVideo = () => {
       <DarkOverlay />
       <Title variants={container} initial="hidden" animate="show" >
         <div>
-          <motion.h1  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.13" data-scroll-speed="4">
+          <motion.h1  variants={item} data-scroll data-scroll-delay="0.13" data-scroll-speed="4">
             E
           </motion.h1>
-          <motion.h1  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.13" data-scroll-speed="4">
+          <motion.h1  variants={item} data-scroll data-scroll-delay="0.13" data-scroll-speed="4">
             l
           </motion.h1>
-          <motion.h1  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.09" data-scroll-speed="4">
+          <motion.h1  variants={item} data-scroll data-scroll-delay="0.09" data-scroll-speed="4">
             e
           </motion.h1>
-          <motion.h1  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.09" data-scroll-speed="4">
+          <motion.h1  variants={item} data-scroll data-scroll-delay="0.09" data-scroll-speed="4">
             g
           </motion.h1>
-          <motion.h1  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.06" data-scroll-speed="4">
+          <motion.h1  variants={item} data-scroll data-scroll-delay="0.06" data-scroll-speed="4">
             a
           </motion.h1>
-          <motion.h1  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.06" data-scroll-speed="4">
+          <motion.h1  variants={item} data-scroll data-scroll-delay="0.06" data-scroll-speed="4">
             n
           </motion.h1>
-          <motion.h1  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.06" data-scroll-speed="4">
+          <motion.h1  variants={item} data-scroll data-scroll-delay="0.06" data-scroll-speed="4">
             z
           </motion.h1>
-          <motion.h1  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.04" data-scroll-speed="4">
+          <motion.h1  variants={item} data-scroll data-scroll-delay="0.04" data-scroll-speed="4">
             i
           </motion.h1>
-          <motion.h1  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.04" data-scroll-speed="4">
+          <motion.h1  variants={item} data-scroll data-scroll-delay="0.04" data-scroll-speed="4">
             a
           </motion.h1>
         </div>
-        <h2  variants={item} initial="hidden" animate="show" data-scroll data-scroll-delay="0.04" data-scroll-speed="2" >Charm In Every Choice</h2>
+        <motion.h2  variants={item} data-scroll data-scroll-delay="0.04" data-scroll-speed="2" >Charm In Every Choice</motion.h2>
       </Title>
       <video src={MainVideo} type="video/mp4" autoPlay muted loop />
     </VideoContainer>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import {motion} from 'framer-motion'
 
-const NavContainer = styled.div`
+const NavContainer = styled(motion.div)`
 width: 100vw;
 z-index: 6;
 position: absolute;
-top: 0;
+top: ${props => props.toggleNavBar ? '0': `-${props.theme.navHeight}`};
 
 display: flex;
 justify-content: center;
 align-items: center;
 
+transition: all 0.3s ease;
+
 `
 
-const MenuItems = styled.ul`
+const MenuItems = styled(motion.ul)`
 position: relative;
 height: ${props => props.theme.navHeight};
 background-color: ${props => props.theme.body};
@@ -54,23 +57,62 @@ text-transform: uppercase;
 cursor: pointer;
 `
 
-const MenuItem = styled.li`
+const MenuItem = styled(motion.li)`
   text-transform: uppercase;
   color: ${props => props.theme.text};
 `
 
+const itemVariants = {
+  hidden : {
+
+  }
+}
 
 const NavBar = () => {
+
+  const [toggleNavBar, setToggleNavBar] = useState(false)
+
   return (
-    <NavContainer>
-      <MenuItems>
-      <MenuBtn>
+    <NavContainer
+    initial={{
+       y: '-100%',
+    }}
+    animate={{
+      y: 0
+    }}
+    transition={{
+      duration: 2,
+      delay: 2,
+    }}
+    toggleNavBar={toggleNavBar}>
+      <MenuItems
+       drag="y"
+       dragConstraints={{
+        top: 0,
+        bottom: 70,
+       }}
+       dragElastic={0.05}
+       dragSnapToOrigin
+       >
+      <MenuBtn onClick={() => setToggleNavBar(!toggleNavBar)}>
         Menu
       </MenuBtn>
-      <MenuItem>Home</MenuItem>
-      <MenuItem>About</MenuItem>
-      <MenuItem>Shop</MenuItem>
-      <MenuItem>New Arrival</MenuItem>
+      <MenuItem
+      whileHover={{scale:1.1, y: -5}}
+      whileTap={{scale: 0.9, y:0}}
+      >Home</MenuItem>
+      <MenuItem
+      whileHover={{scale:1.1, y: -5}}
+      whileTap={{scale: 0.9, y:0}}
+      >About</MenuItem>
+      <MenuItem
+      whileHover={{scale:1.1, y: -5}}
+      whileTap={{scale: 0.9, y:0}}
+      >Shop</MenuItem>
+      <MenuItem
+      whileHover={{scale:1.1, y: -5}}
+      whileTap={{scale: 0.9, y:0}}
+      >New Arrival</MenuItem>
       </MenuItems>
     </NavContainer>
   )
